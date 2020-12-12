d2a629a7522f6013c659603f05469fff07cc6062
ppttfish <ppttfish@163.com>
2020-12-12 17:00:54 +0800

    feat: heroku init

--- bin/www.js
+++ bin/www.js
@@ -1,7 +1,9 @@
 const http = require('http')
 const serverHandle = require('../app')
-const port = 8001
+const port = process.env.PORT || config.port
 
 const server = http.createServer(serverHandle)
 
-server.listen(port)
+server.listen(port, function(){
+  console.log(`${pkg.name} listening on port ${port}`)
+})
